6dbe49bde1209d10b38ae1e971590ccfb2ec335a
srikarkrishna <srikarkrishna999@gmail.com>
2017-11-05 08:21:05 +0530

    assignment-1 finished

---
+++ src/app/shared/dishes.ts
@@ -0,0 +1,14 @@
+export class Dish{
+	name : string;
+	image : string;
+	category : string;
+	label : string;
+	price : string;
+	description : string;
+	comments : {
+	   rating : string;
+	   comment : string;
+	   author : string;
+	   date : string;
+	}
+}
